864a5b63a3f6626de255ddfb29b1632ba15f3ec8
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-03-06 05:27:31 +0100

    Añade tests de Biblioteca y actualiza la documentación

--- src/test/biblioteca/BibliotecaTest.java
+++ src/test/biblioteca/BibliotecaTest.java
@@ -1,18 +1,57 @@
 package biblioteca;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Clase de pruebas unitarias para la clase {@link Biblioteca}
+ * @see Biblioteca
+ */
 
 class BibliotecaTest {
 
-    @org.junit.jupiter.api.Test
+    private Biblioteca biblioteca;
+    private Libro libro;
+
+    @BeforeEach
+    public void setUp() {
+        biblioteca = new Biblioteca();
+        libro = new Libro("Neuromante", "William Gibson", 1984);
+    }
+
+    @Test
     void agregarLibro() {
+        // Dado
+        boolean resultado = biblioteca.agregarLibro(libro);
+        // Entonces
+        assertTrue(resultado, "Libro agregado con éxito.");
+        assertTrue(biblioteca.getLibros().contains(libro), "La biblioteca contiene el libro agregado.");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void eliminarLibro() {
+        // Dado
+        boolean libroAnadido = biblioteca.agregarLibro(libro);
+        // Cuando
+        boolean resultado = biblioteca.eliminarLibro(libro);
+        // Entonces
+        assertFalse(biblioteca.getLibros().contains(libro));
     }
 
-    @org.junit.jupiter.api.Test
-    void encuentraLibroPorTitulo() {
+
+    /**
+     * Test para verificar que el método encuentraLibroPorTitulo devuelve null
+     * si no encuentra un libro con el título dado.
+     */
+    @Test
+    public void testEncuentraLibroPorTitulo_NoExistente() {
+        // Cuando
+        Libro libroEncontrado = biblioteca.encuentraLibroPorTitulo("Conde Cero");
+        // Entonces
+        assertNull(libroEncontrado);
     }
 }
